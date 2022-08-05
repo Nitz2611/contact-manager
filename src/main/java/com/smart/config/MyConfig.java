40d7312946e396b1de99d04ec63354b798bc026a
Nitish sharma <75838617+Nitz2611@users.noreply.github.com>
2022-08-05 18:34:48 +0530

    mapped root "/" to index.html

--- src/main/java/com/smart/config/MyConfig.java
+++ src/main/java/com/smart/config/MyConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -56,6 +58,13 @@ public class MyConfig extends WebSecurityConfigurerAdapter{
 		.defaultSuccessUrl("/user/index")
 		.and().csrf().disable();
 	}
+	public class WebConfiguration implements WebMvcConfigurer {
+
+		   @Override
+		   public void addViewControllers(ViewControllerRegistry registry) {
+		       registry.addViewController("/").setViewName("forward:/index.html");
+		   }
 	
+}
 	
 }
