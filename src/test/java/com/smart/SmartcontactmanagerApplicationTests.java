25d6fe89bdf8bfe18d1787ae698be088e24ea907
Nitish sharma <75838617+Nitz2611@users.noreply.github.com>
2023-12-24 17:24:06 +0530

    Update SmartcontactmanagerApplicationTests.java

--- src/test/java/com/smart/SmartcontactmanagerApplicationTests.java
+++ src/test/java/com/smart/SmartcontactmanagerApplicationTests.java
@@ -3,7 +3,7 @@ package com.smart;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
-
+// accha
 @SpringBootTest
 class SmartcontactmanagerApplicationTests {
 
